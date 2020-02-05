47471838c85eb0a38130e8f693c21ce495b3d5b9
TheDemon2101 <eliel2101@hotmail.com>
2020-02-04 23:02:42 -0600

    Primer avance de login

---
+++ proyecto_vue/proyecto_vue/src/components/LoginForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <h3 class="text-right">Hellow</h3>
+    <h3 class="text-right">Welcome Back</h3>
+    <div class="col">
+      <input
+        type="email"
+        class="from-control mb-2"
+      >
+      <input
+        type="contrasena"
+        class="form-control mb-2"
+      >
+      <button class="btn btn-purple btn-blocck">Login</button>
+    </div>
+  </section>
+</template>
+
+<script lang="js">
+export default {
+  name: 'LoginForm'
+}
+</script>
+
+<style lang="scss">
+.btn-purple {
+  background: purple;
+  &:hover,
+  &:active {
+    background: green;
+  }
+}
+</style>
---
+++ proyecto_vue/proyecto_vue/src/views/LogIn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login container">
+    <div class="card p-2">
+      <div class="row">
+        <div class="col">
+          <LoginForm></LoginForm>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+
+import LoginForm from '@/components/LoginForm'
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
